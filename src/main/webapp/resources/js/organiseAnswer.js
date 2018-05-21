f563623ee0912132456a147d96fc249d4cf26c2a
vitaliy <bliznuuk@gmail.com>
2018-05-21 17:19:43 +0300

    Organize response using JavaScript with jQuery. Add style to the main.css

--- src/main/webapp/resources/js/organiseAnswer.js
+++ src/main/webapp/resources/js/organiseAnswer.js
@@ -1,2 +1,25 @@
+let ageDiffer = $('#mainForm\\:ageDiff').text();
+console.log(ageDiffer);
 
-$('#answer\\:ageDiff').css({'display': 'inline-block'});
+if (ageDiffer === 0) {
+    $('#mainForm\\:sameAge').css({'display': 'inline'});
+}
+
+else{
+    console.log($('#mainForm\\:absAgeDiff').text());
+    $('#mainForm\\:absAgeDiff').css({'display': 'inline'});
+    
+    if($('#mainForm\\:absAgeDiff').text() > 1) {
+        $('#mainForm\\:years').css({'display': 'inline'});
+    }
+    else {
+        $('#mainForm\\:year').css({'display': 'inline'});
+    }
+    
+    if (ageDiffer < 0) {
+        $('#mainForm\\:younger').css({'display': 'inline'});
+    }
+    else {
+        $('#mainForm\\:older').css({'display': 'inline'});
+    }
+}
